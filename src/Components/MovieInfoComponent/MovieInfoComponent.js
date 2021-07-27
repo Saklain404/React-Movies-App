c0ff8036479f1abc919b765f4a7f9a5317133f56
saklain404 <mohdsaklain8899@gmail.com>
2021-07-27 22:28:25 +0530

    info fixing

--- src/Components/MovieInfoComponent/MovieInfoComponent.js
+++ src/Components/MovieInfoComponent/MovieInfoComponent.js
@@ -20,36 +20,36 @@ const MovieInfoComponent = (props) => {
       <img className="CoverImages" src={movieInfo?.Poster} alt="ggg" />
       <div className="InfoColumns">
         <span className="MovieNames">Movie : {movieInfo?.Title}</span>  
-        <span className="Infos">
-          IMDB Rating: {movieInfo?.imdbRating}
-        </span>
-        <span className="Infos">
-          Year: {movieInfo?.Year}
-        </span>
-        <span className="Infos">
-          Language: {movieInfo?.Language}
-        </span>
-        <span className="Infos">
-          Rated:  {movieInfo?.Rated}
-        </span>
-        <span className="Infos">
-          Release: {movieInfo?.Released}
-        </span>
-        <span className="Infos">
-          Runtime: {movieInfo?.Runtime}
-        </span>
-        <span className="Infos">
-          Genre:{movieInfo?.Genre}
-        </span>
-        <span className="Infos">
+        <div className="Infos">
+          IMDB Rating: <span className="spn">{movieInfo?.imdbRating}</span>
+        </div>
+        <div className="Infos">
+          Year: <span className="spn">{movieInfo?.Year}</span>
+        </div>
+        <div className="Infos">
+          Language: <span className="spn">{movieInfo?.Language}</span>
+        </div>
+        <div className="Infos">
+          Rated: <span className="spn"> {movieInfo?.Rated}</span>
+        </div>
+        <div className="Infos">
+          Release: <span className="spn">{movieInfo?.Released}</span>
+        </div>
+        <div className="Infos">
+          Runtime: <span className="spn">{movieInfo?.Runtime}</span>
+        </div>
+        <div className="Infos">
+          Genre: <span className="spn">{movieInfo?.Genre}</span>
+        </div>
+        <div className="Infos">
           Director: <span className="spn">{movieInfo?.Director}</span>
-        </span>
-        <span className="Infos">
+        </div>
+        <div className="Infos">
           Actors: <span className="spn">{movieInfo?.Actors}</span>
-        </span>
-        <span className="Infos">
+        </div>
+        <div className="Infos">
           Plot: <span className="spn">{movieInfo?.Plot}</span>
-        </span>
+        </div>
       </div>
       <div onClick={()=>props.onMovieSelect()}  className="colseBtn">X</div>
       </> : "Loading..."}
